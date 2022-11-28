efad7a5db0e08d0c5579061aec0924c02e2ce288
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-28 18:06:06 +0800

    Completes lesson 04

--- 01-Basics/04-Webpack/src/script.js
+++ 01-Basics/04-Webpack/src/script.js
@@ -1,1 +1,28 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+
+// SCENE
+const scene = new THREE.Scene();
+
+// MESH
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xddd });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// SIZES
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// RENDERER
+const canvas = document.querySelector('canvas.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
