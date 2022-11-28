808e3feea06a41a955ce8599c98492facc824711
Peyton Bechard <pbechard@my.yorku.ca>
2022-11-28 16:38:30 +0800

    Adds init files for 03-Basic_Scene lesson

---
+++ 01-Basics/03-Basic_Scene/script.js
@@ -0,0 +1,6 @@
+// SCENE
+const scene = new THREE.Scene();
+
+// MESH = GEOMETRY + MATERIAL
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
