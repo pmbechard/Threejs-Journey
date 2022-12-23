deca719dc4e882a5a2d7f52ab788177136c05b74
Peyton Bechard <pbechard@my.yorku.ca>
2022-12-23 15:20:34 +0800

    Completes lesson 26

--- 03_Advanced-Techniques/26_Code-Structuring/src/Experience/Experience.js
+++ 03_Advanced-Techniques/26_Code-Structuring/src/Experience/Experience.js
@@ -1,9 +1,14 @@
 import * as THREE from 'three';
 
+import Debug from './Utils/Debug';
 import Sizes from './Utils/Sizes';
 import Time from './Utils/Time';
+import Resources from './Utils/Resources';
 import Camera from './Camera';
 import Renderer from './Renderer';
+import World from './World/World';
+
+import sources from './sources';
 
 let instance = null;
 
@@ -20,11 +25,14 @@ export default class Experience {
     this.canvas = canvas;
 
     // SETUP
+    this.debug = new Debug();
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
+    this.resources = new Resources(sources);
     this.camera = new Camera();
     this.renderer = new Renderer();
+    this.world = new World();
 
     // RESIZE EVENT LISTENER
     this.sizes.on('resize', () => {
@@ -44,6 +52,29 @@ export default class Experience {
 
   update() {
     this.camera.update();
+    this.world.update();
     this.renderer.update();
+  }
+
+  destroy() {
+    this.sizes.off('resize');
+    this.time.off('tick');
+
+    this.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose();
+
+        for (const key in child.material) {
+          const value = child.material[key];
+          if (value && typeof value.dispose === 'function') {
+            value.dispose();
+          }
+        }
+      }
+    });
+
+    this.camera.controls.dispose();
+    this.renderer.instance.dispose();
+    if (this.debug.active) this.debug.ui.destroy();
   }
 }
