835464e9c4976a1d81d19e2ac7356b7813de7c3a
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-14 21:34:30 +0800

    Begins lesson 39

---
+++ 06_Portal-Scene/39_Adding-Details-to-the-Scene/src/script.js
@@ -0,0 +1,147 @@
+import './style.css';
+import * as dat from 'lil-gui';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI({
+  width: 400,
+});
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader();
+
+// Draco loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('draco/');
+
+// GLTF loader
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+// --> TEXTURES
+const bakedTexture = textureLoader.load('baked.jpg');
+bakedTexture.flipY = false;
+bakedTexture.encoding = THREE.sRGBEncoding;
+
+// --> MATERIALS
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture });
+
+// -> Lamp Material
+const lampMaterial = new THREE.MeshBasicMaterial({
+  color: 0xebd099,
+});
+
+// -> Portal Material
+const portalMaterial = new THREE.MeshBasicMaterial({
+  color: 0xdfd6ff,
+});
+
+// --> MODEL
+gltfLoader.load('portal.glb', (gltf) => {
+  const portalMesh = gltf.scene.children.find(
+    (child) => child.name === 'Portal'
+  );
+  portalMesh.material = portalMaterial;
+
+  const lampRightMesh = gltf.scene.children.find(
+    (child) => child.name === 'Lamp'
+  );
+  lampRightMesh.material = lampMaterial;
+
+  const lampLeftMesh = gltf.scene.children.find(
+    (child) => child.name === 'Lamp001'
+  );
+  lampLeftMesh.material = lampMaterial;
+
+  const bakedMesh = gltf.scene.children.find((child) => child.name === 'baked');
+  bakedMesh.material = bakedMaterial;
+
+  scene.add(gltf.scene);
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 4;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
