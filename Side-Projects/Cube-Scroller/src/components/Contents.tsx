af9e2d03c948e7530320b952853ade2eefcdc18a
Peyton Bechard <peyton.bechard@gmail.com>
2023-02-18 09:16:33 +0800

    Removes unused envMaps from cube scroller side project

--- Side-Projects/Cube-Scroller/src/components/Contents.tsx
+++ Side-Projects/Cube-Scroller/src/components/Contents.tsx
@@ -18,8 +18,9 @@ const Contents: React.FC<Props> = ({ side }) => {
       gsap.to(textRef.current.position, { y: 0, duration: 1, delay: 1.5 });
     }
     if (bgRef.current) {
-      gsap.to(bgRef.current.scale, { x: 0, y: 0, duration: 0.5 });
-      gsap.to(bgRef.current.scale, { x: 1, y: 1, duration: 1, delay: 1.5 });
+      gsap.to(bgRef.current.scale, { x: 1, y: 0, duration: 0.1 });
+      gsap.to(bgRef.current.scale, { x: 0, y: 0, duration: 0.1, delay: 0.1 });
+      gsap.to(bgRef.current.scale, { x: 1, y: 1, duration: 0.1, delay: 1.8 });
     }
     if (side == 0) setText('Peyton Bechard');
     else if (side == 1) setText('About Me');
@@ -32,7 +33,7 @@ const Contents: React.FC<Props> = ({ side }) => {
   return (
     <>
       <mesh ref={bgRef} position={[0, 0, 0.6]} rotation={[Math.PI * -2, 0, 0]}>
-        <planeGeometry args={[0.9, 0.9, 32, 32]} />
+        <planeGeometry args={[1.1, 1.1, 16, 16]} />
         <MeshTransmissionMaterial
           transmission={3}
           distortionScale={3}
@@ -41,7 +42,7 @@ const Contents: React.FC<Props> = ({ side }) => {
           color={'#003300'}
         />
       </mesh>
-      <Text ref={textRef} fontSize={0.05} position={[0, -3, 1]}>
+      <Text ref={textRef} fontSize={0.05} position={[0, -3, 0.8]}>
         {text}
         <meshStandardMaterial color={'mediumpurple'} />
       </Text>
