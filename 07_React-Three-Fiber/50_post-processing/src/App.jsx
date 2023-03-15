f6b23c2bbd65945ecb64a6bb718d6038f8ae70ad
Peyton Bechard <peyton.bechard@gmail.com>
2023-03-15 20:37:17 +0800

    Continues lesson 50

--- 07_React-Three-Fiber/50_post-processing/src/App.jsx
+++ 07_React-Three-Fiber/50_post-processing/src/App.jsx
@@ -1,23 +1,44 @@
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
 import { Perf } from 'r3f-perf';
 import { OrbitControls } from '@react-three/drei';
+import {
+  EffectComposer,
+  Glitch,
+  Noise,
+  Vignette,
+} from '@react-three/postprocessing';
+import { BlendFunction, GlitchMode } from 'postprocessing';
 
 const App = () => {
-  const cube = useRef();
-
-  useFrame((state, delta) => {
-    cube.current.rotation.y += delta * 0.2;
-  });
-
   return (
     <>
       <Perf position={'top-left'} />
       <OrbitControls makeDefault />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
 
-      <mesh ref={cube} position-x={2} scale={1.5}>
+      <color arg={['#ffffff']} attach={'background'} />
+
+      <EffectComposer multisampling={8}>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.4]}
+          mode={GlitchMode.SPORADIC}
+        /> */}
+
+        {/* <Noise premultiply blendFunction={BlendFunction.AVERAGE} /> */}
+
+        
+      </EffectComposer>
+
+      <ambientLight intensity={0.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+
+      <mesh position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color={'mediumpurple'} />
       </mesh>
